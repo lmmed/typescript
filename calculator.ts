730183700a4097ca3246d2fc7f82707b023f9d4b
Gaetan Guillot <gguillot@openstudio.fr>
2023-12-13 14:33:26 +0100

    Simple calculator and AdditionList

--- calculator.ts
+++ calculator.ts
@@ -1,36 +1,30 @@
-const addition = (numbers:number[]):number => {
-  if(numbers.length === 0){
-    throw new Error("Il faut au minium un paramètre");
+const addition = (a:number, b: number): number => {
+  return a + b;
+}
+
+const substraction =(a:number, b: number): number => {
+  return a - b;
+}
+
+const multiplication =(a:number, b: number): number => {
+  return a * b;
+}
+
+const division = (a:number, b: number):number => {
+  if(a===0 || b === 0) {
+    throw new Error("Division par 0 impossible");
   }
-  return numbers.reduce((old, current) => old + current, 0);
+  return a / b;
 };
 
-const substraction = (numbers:number[]):number => {
-  if(numbers.length === 0){
-    throw new Error("Il faut au minium un paramètre");
-  }
-  return numbers.reduce((old, current, index) => index === 0 ? current : old - current, 0);
-};
-
-const multiplication = (numbers:number[]):number => {
-  if(numbers.length === 0){
-    throw new Error("Il faut au minium un paramètre");
-  }
-  return numbers.reduce((old, current, index) => index === 0 ? current : old * current, 0);
-};
-
-const division = (numbers:number[]):number => {
-  if(numbers.length === 0){
-    throw new Error("Il faut au minium un paramètre");
-  }
-  if(numbers.includes(0)){
-    throw new Error("Attention division par 0 interdite");
-  }
-  return numbers.reduce((old, current, index) => index === 0 ? current : old / current, 0);
+const additionList = (...numbers:number[]):number => {
+  return numbers.reduce((old, current, index) => old + current, 0);
 };
 
 
-const additionResult = addition([5,1,4]);
-const substractionResult = substraction([10, 5, 2])
-const multiplicationResult = multiplication([10, 2]);
-const divisionResult = division([50,5,2]);
+
+const additionResult = addition(5,4);
+const substractionResult = substraction(10,5)
+const multiplicationResult = multiplication(5,2);
+const divisionResult = division(50,2);
+const additionListResult = additionList(1,2,5);
