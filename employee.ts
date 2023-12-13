3334f7334628527076348378c787a7a7462a2460
Lucanis <damien.foulhoux@gmail.com>
2023-12-13 16:27:24 +0100

    add payment

--- employee.ts
+++ employee.ts
@@ -1,12 +1,34 @@
-class Group {
+interface CanPay {
+  sendPayment(): void;
+}
+interface CanBePaid {
+  toBePaid: number;
+  receivePayment(): void;
+}
+
+class Group implements CanPay {
   name: string;
-  elements: Employee[];
+  employees: Employee[];
+  suppliers: Supplier[];
   constructor(name: string) {
     this.name = name;
-    this.elements = [];
+    this.employees = [];
+    this.suppliers = [];
   }
-  add(employee: Employee) {
-    this.elements.push(employee);
+  addEmployee(employee: Employee) {
+    this.employees.push(employee);
+  }
+  addSupplier(supplier: Supplier) {
+    this.suppliers.push(supplier);
+  }
+  sendPayment(): void {
+    console.log("pay up group");
+    this.employees.forEach((employee) => {
+      employee.receivePayment();
+    });
+    this.suppliers.forEach((employee) => {
+      employee.receivePayment();
+    });
   }
 }
 
@@ -20,17 +42,36 @@ class Person {
   }
 }
 
-class Employee {
+class Supplier implements CanBePaid {
+  readonly name: string;
+  toBePaid: number;
+
+  constructor(name: string) {
+    this.name = name;
+    this.toBePaid = 0;
+  }
+
+  receivePayment(): void {
+    console.log("pay supplier");
+  }
+}
+
+class Employee implements CanBePaid {
   id: number;
   person: Person;
-  group: Group | null;
+  #group: Group | null;
+  toBePaid: number;
   constructor(id: number, person: Person, group: Group | null) {
     this.id = id;
     this.person = person;
-    this.group = group;
+    this.#group = group;
+    this.toBePaid = 0;
     if (group) {
-      group?.add(this);
+      group?.addEmployee(this);
     }
+  }
+  receivePayment(): void {
+    console.log("pay employee");
   }
 }
 
@@ -43,4 +84,10 @@ const jack = new Person("Jack", "Riley");
 const employee1 = new Employee(1, mo, marketing);
 const employee2 = new Employee(2, jack, finance);
 
-console.log(employee1.group?.elements, finance.elements);
+const paperClipMaker = new Supplier("paperclip");
+
+marketing.addSupplier(paperClipMaker);
+finance.addSupplier(paperClipMaker);
+
+finance.sendPayment();
+finance.sendPayment();
