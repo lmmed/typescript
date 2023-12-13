abb3d4d4b45e6ffe1c8a397e4e3b1af5afadb394
Lucanis <damien.foulhoux@gmail.com>
2023-12-13 16:01:03 +0100

    add group exercise

--- employee.ts
+++ employee.ts
@@ -1,23 +1,44 @@
-interface Payable {
-  sendPayment(): void;
-}
-class Supplier implements Payable {
-  sendPayment(): void {
-    console.log("Supplier");
+class Group {
+  name: string;
+  elements: Person[];
+  constructor(name: string) {
+    this.name = name;
+    this.elements = [];
   }
-}
-class Employee implements Payable {
-  sendPayment(): void {
-    console.log("Employee");
-  }
-}
-
-class Company {
-  sendPayments(toBePaid: Payable[]) {
-    toBePaid.forEach((p) => p.sendPayment());
+  add(person: Person) {
+    this.elements.push(person);
   }
 }
 
-const company = new Company();
+class Person {
+  readonly firstName: string;
+  readonly lastName: string;
 
-company.sendPayments([new Employee(), new Supplier()]);
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+class Employee {
+  person: Person;
+  group: Group | null;
+  constructor(person: Person, group: Group | null) {
+    this.person = person;
+    this.group = group;
+    if (group) {
+      group?.add(this.person);
+    }
+  }
+}
+
+const marketing = new Group("marketing");
+const finance = new Group("finance");
+
+const mo = new Person("Mohammed", "Lamhaouar");
+const jack = new Person("Jack", "Riley");
+
+const employee1 = new Employee(mo, marketing);
+const employee2 = new Employee(jack, finance);
+
+console.log(employee1.group?.elements);
