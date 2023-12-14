a325f59e5a7dc2234eb7fcf8ebf8da23c5b27d24
Lucanis <damien.foulhoux@gmail.com>
2023-12-14 11:04:31 +0100

    add remotefetch

--- services/EmployeeDirectory.ts
+++ services/EmployeeDirectory.ts
@@ -1,4 +1,20 @@
 import { Employee } from "../models/employee";
 import { PhoneBook } from "../models/phonebook";
 
-export class EmployeePhoneBook extends PhoneBook<Employee> {}
+export class EmployeePhoneBook extends PhoneBook<Employee> {
+  getEmployeesFromRemote() {
+    return fetch(
+      "https://657aa1961acd268f9afb6388.mockapi.io/api/emplyee/all",
+      {
+        signal: AbortSignal.timeout(5000),
+      }
+    )
+      .then((res) => res.json())
+      .then((list) => {
+        this.items = list;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+}
