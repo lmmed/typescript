976e624b2b91be1dd46ff39e720e3f7cacdb4dc1
Mohamed LAMHAOUAR <mohamed.lamhaouar@gmail.com>
2023-12-14 11:11:28 +0100

    Add files via upload

--- company.ts
+++ company.ts
@@ -1,81 +1,12 @@
-interface CanPay{
-    sendPayment(): void;
-}
-abstract class Employee implements CanPay{
-    constructor( 
-        readonly firstName: string, 
-        readonly lastName: string,
-        readonly salaire: number
-      ) {} 
-      sendPayment(): void {
-          console.log(`pay ${this.firstName} --- salaire ${this.salaire}`);
-      }
 
-      abstract calculerSalaire(): number;
-}
-class Vendeur extends Employee { 
-    constructor( 
-      public readonly firstName: string, 
-      public readonly lastName: string, 
-      public readonly salaire: number, 
-      public readonly commission: number
-    ) { 
-      super(firstName, lastName, salaire); 
-    } 
-    calculerSalaire(): number{
-        return this.salaire + (this.commission/100)* this.salaire;
-    }
-  }
 
-  class Developpeur extends Employee { 
-    calculerSalaire(): number{
-        return this.salaire;
-    }
-  } 
 
-class Fournisseur implements CanPay{
-    constructor( 
-        readonly name: string,
-        readonly toBePaid: number
-      ) {} 
-      sendPayment(): void {
-          console.log(`pay ${this.name} --- facture ${this.toBePaid}`);
-      }
-}
-class Team{
-    #employees: Employee[]= [];
-    constructor(readonly name: string){}
-    addMember(employee: Employee){
-        this.#employees.push(employee);
-    }
-    showAll(){
-        this.#employees.forEach(
-            employee => console.log(employee)
-            
-        );
-    }
-}
 
-class Company{
-    sendPayment(toBePaid: CanPay[]){
-        toBePaid.forEach(p => p.sendPayment());
-    }
-}
+  
 
-const marketing = new Team("Marketing");
-const finance = new Team("Finance");
-marketing.addMember(new Developpeur("momo", "lm", 2000));
-finance.addMember(new Developpeur("jiij", "jiji", 1500));
 
-const company = new Company();
-company.sendPayment([
-    new Vendeur("momo", "lm", 2000, 5),
-    new Developpeur("jiij", "jiji", 1500),
-    new Fournisseur("LeroyMerlin", 3000)
-]);
+import { EmployeeDirectoryService } from "./service/EmployeeDirectoryService";
 
-marketing.showAll();
-finance.showAll();
+const employeServcie = new EmployeeDirectoryService();
 
-console.log(marketing);
-console.log(finance);
+employeServcie.recupererEmploye();
