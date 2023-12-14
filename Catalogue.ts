0a8e8b0ec5d1327b875235fc80232ae9f67574ad
Yann <anodevfr@gmail.com>
2023-12-14 15:09:09 +0100

    boo

---
+++ Catalogue.ts
@@ -0,0 +1,25 @@
+class Catalogue {
+    chercherProduit() {
+
+    }
+
+    chercherProduitParDesignation() {
+
+    }
+
+    chercherProduitParFamille() {
+
+    }
+
+    changer(ref: string, prix: number) {
+
+    }
+
+    ajouterProduit(ref: string, prix: number, famille: any) {
+
+    }
+
+    supprimerproduit(ref: string) {
+
+    }
+}
