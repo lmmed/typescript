a2a26162706238a3f51c2968d9f5952debd11e5f
lnormandon <lnormandon@openstudio.fr>
2023-12-15 12:22:04 +0100

    tests error & promise

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,28 @@ function add (a:number,b:number) {
     return a+b;
 }
 
+function error() {
+    throw Error;
+}
+
+function promisFunction() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('toto');
+        }, 1000);
+    });
+}
+
 test('test', () => {
-    expect(add(1,1)).toBe(2);
+    expect(add(5.5,5.2)).toBeCloseTo(10.7);
+} );
+
+test('test error', () => {
+    expect(() => {error()}).toThrow();
+} );
+
+test('test promise', () => {
+   return promisFunction().then(data => {
+    expect(data).toMatch('toto');
+   })
 } );
