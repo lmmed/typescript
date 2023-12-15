6365ff14eaddf6d5759396e95223c5e9427c6dc5
Cédric Robert <crobert@openstudio.fr>
2023-12-15 11:53:41 +0100

    add all operations test

--- operation.test.ts
+++ operation.test.ts
@@ -1,5 +1,25 @@
-import { add } from "./operations";
+import { add, multiply, divide, substract } from "./operations";
 
-test("simple test", () => {
+test("add", () => {
   expect(add(1, 1)).toBe(2);
+  expect(add(5, -1)).toBe(4);
+  expect(add(0, 0)).toBe(0);
+  expect(add(-1, -1)).toBe(-2);
+});
+
+test("multiply", () => {
+  expect(multiply(1, 1)).toBe(1);
+  expect(multiply(2, 1)).toBe(2);
+  expect(multiply(2, -1)).toBe(-2);
+});
+test("substract", () => {
+  expect(substract(1, 1)).toBe(0);
+  expect(substract(0, 0)).toBe(0);
+  expect(substract(5, 1)).toBe(4);
+  expect(substract(5, -1)).toBe(6);
+});
+test("divide", () => {
+  expect(divide(1, 1)).toBe(1);
+  expect(divide(0, 1)).toBe(0);
+  expect(() => divide(1, 0)).toThrow("cannot divide by 0");
 });
