409aef3460db2f9e1459ea0443235080b37016d9
Cédric Robert <crobert@openstudio.fr>
2023-12-15 12:01:10 +0100

    add tobelessthan

--- operation.test.ts
+++ operation.test.ts
@@ -11,6 +11,7 @@ test("multiply", () => {
   expect(multiply(1, 1)).toBe(1);
   expect(multiply(2, 1)).toBe(2);
   expect(multiply(2, -1)).toBe(-2);
+  expect(multiply(2, -1)).toBeLessThan(0);
 });
 test("substract", () => {
   expect(substract(1, 1)).toBe(0);
@@ -21,5 +22,6 @@ test("substract", () => {
 test("divide", () => {
   expect(divide(1, 1)).toBe(1);
   expect(divide(0, 1)).toBe(0);
+  expect(divide(1, 10)).toBeLessThan(1);
   expect(() => divide(1, 0)).toThrow("cannot divide by 0");
 });
