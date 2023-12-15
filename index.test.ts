97a8f0acd0ad212cd219c6d2155c0115eb8b2221
Cédric Robert <crobert@openstudio.fr>
2023-12-15 12:08:42 +0100

    add array test

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,14 @@
-test("random test", () => {
+test("random string test", () => {
   const password = "pizza123";
   expect(password).toMatch(/.{7,}/);
   expect(password).toMatch(/(?=\d)/);
   expect(password).toMatch(/(?=[a-z])/);
   expect(password).not.toMatch(/(?=[A-Z])/);
+});
+
+test("random array test", () => {
+  const list = ["banana", "pear", "cherry"];
+  expect(list).toContain("banana");
+  expect(list).toContain("pear");
+  expect(list).not.toContain("dog");
 });
